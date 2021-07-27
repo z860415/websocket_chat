c73e7733e5e24d2f72e15cfeb8e765fa270fd785
eric_li <eric_li@mail.chungyo.net>
2021-07-27 17:24:11 +0800

    update

--- main.py
+++ main.py
@@ -59,5 +59,4 @@ routes = [
 app = FastAPI(routes=routes)
 
 if __name__ == '__main__':
-    uvicorn.run(app='main:app', host="0.0.0.0",
-                port=8000, reload=True, debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, debug=True)
